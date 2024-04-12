149caefb796dced948fbdcf795f3a400daa1ab57
ninamanas <ninamanasievska@gmail.com>
2024-04-12 20:17:58 +0200

    Popraveno

--- SILab1Main.java
+++ SILab1Main.java
@@ -14,7 +14,11 @@ public class SILab1Main {
      
             list.add(sc.next());
         }
+<<<<<<< HEAD
 System.out.println(SILab1_1.filterOddLengthStrings(list));
 
+=======
+System.out.println(SILab1_2.makeBigStringFrom(list));
+>>>>>>> 6194846 ( Call method makeBigStringFrom in main class.)
     }
 }
